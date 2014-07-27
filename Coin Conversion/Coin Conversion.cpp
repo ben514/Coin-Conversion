c2f7b3787d6503ab7e275407b927a57a6c916329
Ben Choi <bjchoi514@gmail.com>
2014-07-26 23:25:24 -0700

    1.0

---
+++ Coin Conversion/Coin Conversion.cpp
@@ -0,0 +1,12 @@
+// Coin Conversion.cpp : Defines the entry point for the console application.
+// Write a program that prompts the user to enter some number of pennies nickels dimes quarters half dollars and one dollar 
+// coins query the user separately for the number of each size coin.
+
+#include "stdafx.h"
+#include "std_lib_facilities.h"
+
+int main()
+{
+	vv
+}
+
